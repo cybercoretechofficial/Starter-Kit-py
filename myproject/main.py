1b4b9b26fd90d7656505631ed8313a7384845ac2
cybercoretechofficial <cybercoretechofficial@gmail.com>
2025-08-31 11:30:29 +0530

    Create main.py

---
+++ myproject/main.py
@@ -0,0 +1,10 @@
+from myproject.module1 import greet
+from myproject.utils.helpers import add_numbers
+
+def main():
+    print("🚀 Welcome to MyProject!")
+    print(greet("CyberX"))
+    print("5 + 10 =", add_numbers(5, 10))
+
+if __name__ == "__main__":
+    main()
